14106acd9e6bf99a573255dd5717265fe42d7014
Baptiste Lemarcis <baptiste.lemarcis@outlook.com>
2018-02-23 19:00:40 -0500

    Initial commit

---
+++ Core.lua
@@ -0,0 +1,18 @@
+function CrimeSpreeDetailsMenuComponent:populate_tabs_data(tabs_data)
+	table.insert(tabs_data, {
+		name_id = "menu_cs_modifiers",
+		page_class = "nothing",
+		width_multiplier = 1
+	})
+	if not self:_is_in_preplanning() then
+		table.insert(tabs_data, {
+			name_id = "menu_cs_rewards",
+			page_class = "nothing",
+			width_multiplier = 1
+		})
+	end
+end
+
+Hooks:PostHook( CrimeSpreeTweakData, "init", "BLEH", function(self, tweak_data)
+	self.crash_causes_loss = false
+end)
